8facbdf3ff9b558514e707b6af6a3d5349bc745b
Superslownic <isuperslownic@gmail.com>
2024-05-10 18:27:05 +0400

    FIx texture

---
+++ Assets/Scripts/Core/Water/WaterMoveSplashAbility.cs
@@ -0,0 +1,12 @@
+using Scripts.Units;
+using UnityEngine;
+
+namespace Scripts.Core
+{
+	public class WaterMoveSplashAbility : Ability
+	{
+		[SerializeField] private GameObject _waterSplashPrefab;
+
+		
+	}
+}
