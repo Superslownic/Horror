ae515b7430e312aea17bf226db340053672360cc
Superslownic <isuperslownic@gmail.com>
2024-06-09 16:59:36 +0400

    Water movement fix

--- Assets/Scripts/Core/Player/Abilities/Swim/WaterMoveAbility.cs
+++ Assets/Scripts/Core/Player/Abilities/Swim/WaterMoveAbility.cs
@@ -7,29 +7,32 @@ namespace Scripts.Core.Player
 {
 	public class WaterMoveAbility : Ability
 	{
+		[SerializeField] private Rigidbody _rigidbody;
 		[SerializeField] private float _speed;
 		[SerializeField] private float _maxHeightAboveWater;
 		[SerializeField] private float _popupDepth;
 		[SerializeField] private float _popupSpeed;
+		[SerializeField] private float _acceleration;
 
 		[Inject] private readonly InputManager _inputManager;
 
 		private PlayerHeadAbility _playerHeadAbility;
-		private ChangeVelocityAbility _changeVelocityAbility;
 		private PlayerCheckWaterAbility _checkWaterAbility;
+		private Vector3 _velocity;
 
 		protected override void OnInitialize()
 		{
-			base.OnInitialize();
 			_playerHeadAbility = Unit.GetAbility<PlayerHeadAbility>();
-			_changeVelocityAbility = Unit.GetAbility<ChangeVelocityAbility>();
 			_checkWaterAbility = Unit.GetAbility<PlayerCheckWaterAbility>();
+		}
+
+		protected override void OnActivate()
+		{
+			_velocity = _rigidbody.linearVelocity;
 		}
 
 		protected override void OnUpdate()
 		{
-			base.OnUpdate();
-
 			float playerHeadHeight = _playerHeadAbility.HeadDetachedAnchor.position.y;
 			float waterSurfaceHeight = _checkWaterAbility.WaterSurfaceHeight;
 			Vector2 moveInput = _inputManager.Move.ReadValue<Vector2>();
@@ -46,7 +49,8 @@ namespace Scripts.Core.Player
 			if (playerHeadHeight >= waterSurfaceHeight + _maxHeightAboveWater && clampedInputMotion.y > 0)
 				clampedInputMotion.y = 0;
 
-			_changeVelocityAbility.SetTargetVelocity(clampedInputMotion);
+			_velocity = Vector3.Lerp(_velocity, clampedInputMotion, _acceleration * Time.deltaTime);
+			_rigidbody.linearVelocity = _velocity;
 		}
 	}
 }
